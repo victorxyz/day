b050c8ec8552f7f255acc8d17376e1eaa493e698
victorxyz <victorxyz@gmail.com>
2016-10-13 16:23:32 +0800

    added seeder for beneficiary

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Beneficiary.create([
+  { name: 'Action For AIDS (Singapore)' },
+  { name: 'Agency for Animal Welfare Ltd' },
+  { name: 'Alzheimers Disease Association' },
+  { name: 'Art Therapists Association Singapore' },
+  { name: 'Beyond Social Services' },
+  { name: 'Breast Cancer Foundation' },
+  { name: 'Childrens Cancer Foundation' },
+  { name: 'Childrens Charities Association Of Singapore, The' },
+  { name: 'Guide Dogs Association of the Blind Ltd' },
+  { name: 'HOPE Dog Rescue' },
+  { name: 'Humanitarian Organization for Migration Economics' },
+  { name: 'Association Of Women For Action And Research' },
+  { name: 'Muscular Dystrophy Association (Singapore)' },
+  { name: 'National Arthritis Foundation' },
+  { name: 'Oasis Second Chance Animal Shelter – OSCAS' },
+  { name: 'Singapore Association For The Deaf, The' },
+  { name: 'Save Our Street Dogs (SOSD)' },
+  { name: 'SPCA Singapore' },
+  { name: 'Transient Workers Count Too (TWC2)' }
+])
