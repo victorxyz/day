ed460831147795b6d02cb549af1dabc9f311f154
victorxyz <victorxyz@gmail.com>
2016-10-13 15:51:06 +0800

    toggle of description completed

--- app/assets/javascripts/goals.js
+++ app/assets/javascripts/goals.js
@@ -8,26 +8,12 @@ $(document).on('turbolinks:load', function() {
   // Get the modal
   var modal = document.getElementById('myModal10');
   console.log("test1")
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn10");
+
 
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[1];
 
-  // When the user clicks the button, open the modal
-  btn.onclick = function() {
-      modal.style.display = "block";
-  }
 
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-      modal.style.display = "none";
-  }
 
-  // When the user clicks anywhere outside of the modal, close it
-  document.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-  }
+
 });
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
+  before_action :search
+
   def index
     @goals = Goal.all
 
